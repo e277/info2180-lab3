bbff2f4a1270e7b2f08e2ecf3f9a30b8c77fb7d8
e277 <ezramuir12@gmail.com>
2023-10-10 22:50:53 -0500

    Exercise 5 - Restart the game

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -41,14 +41,7 @@ window.onload = function() {
         })
     })
 
-    // When the user gets three X’s or O’s in a row they are declared the winner. Your next 
-    // task is to check when either X or O has won and update the message on the page to say 
-    // "Congratulations! X is the Winner!" or "Congratulations! O is the Winner!" depending 
-    // on who wins. There is a div with an id of status, that this message should be placed 
-    // in. You should also ensure that the class you-won is added to the status div as well. 
-    
-    // Hint: You can use the JavaScript innerHTML or textContent property to help you to 
-    // place the message in the status div.
+   
     const status = document.getElementById("status")
     let winner = false
     function checkWinner() {
@@ -68,6 +61,19 @@ window.onload = function() {
             }
         }
     }
+
+    
+    const newGame = document.getElementsByClassName("btn")[0]
+    newGame.addEventListener("click", () => {
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].innerHTML = ""
+            squares[i].classList.remove(X_CLASS)
+            squares[i].classList.remove(O_CLASS)
+        }
+        status.innerHTML = "Move your mouse over a square and click to play an X or an O."
+        status.classList.remove("you-won")
+        winner = false
+    })
 
 }
 
