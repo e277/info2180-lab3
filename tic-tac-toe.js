7f6eaac106ce1d95f67dc8edd6cb24ed49f91d1f
e277 <ezramuir12@gmail.com>
2023-10-10 22:42:30 -0500

    Exercise 4 - Check for the winner and update the status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function() {
     const gameBoard = document.getElementById("board")
     // add the class square to each div in the board
     for (let i = 0; i < gameBoard.children.length; i++) {
@@ -14,8 +14,8 @@ window.onload = function(){
     ]
     let xTurn = true
 
-    gameBoard.addEventListener("click", function(event){
-        square = event.target
+    gameBoard.addEventListener("click", (event) => {
+        const square = event.target
         if (xTurn){
             square.innerHTML = X_CLASS
             square.classList.add(X_CLASS)
@@ -25,18 +25,49 @@ window.onload = function(){
             square.classList.add(O_CLASS)
             xTurn = true
         }
+
+        // check if there is a winner
+        checkWinner()
     })
 
-    
+
     const squares = document.querySelectorAll(".square")
     squares.forEach(square => {
-        square.addEventListener("mouseover", function(event) {
+        square.addEventListener("mouseover", () => {
             square.classList.add("hover")
         })
-        square.addEventListener("mouseout", function(event) {
+        square.addEventListener("mouseout", () => {
             square.classList.remove("hover")
         })
     })
+
+    // When the user gets three X’s or O’s in a row they are declared the winner. Your next 
+    // task is to check when either X or O has won and update the message on the page to say 
+    // "Congratulations! X is the Winner!" or "Congratulations! O is the Winner!" depending 
+    // on who wins. There is a div with an id of status, that this message should be placed 
+    // in. You should also ensure that the class you-won is added to the status div as well. 
+    
+    // Hint: You can use the JavaScript innerHTML or textContent property to help you to 
+    // place the message in the status div.
+    const status = document.getElementById("status")
+    let winner = false
+    function checkWinner() {
+        for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+            const winningCombo = WINNING_COMBINATIONS[i]
+            const square1 = squares[winningCombo[0]]
+            const square2 = squares[winningCombo[1]]
+            const square3 = squares[winningCombo[2]]
+            if (square1.innerHTML === X_CLASS && square2.innerHTML === X_CLASS && square3.innerHTML === X_CLASS) {
+                status.innerHTML = "Congratulations! X is the Winner!"
+                status.classList.add("you-won")
+                winner = true
+            } else if (square1.innerHTML === O_CLASS && square2.innerHTML === O_CLASS && square3.innerHTML === O_CLASS) {
+                status.innerHTML = "Congratulations! O is the Winner!"
+                status.classList.add("you-won")
+                winner = true
+            }
+        }
+    }
 
 }
 
